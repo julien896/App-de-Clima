bf775e381dae34613eec64300a98c70421b634a6
julien896 <42101048+julien896@users.noreply.github.com>
2020-04-09 20:02:30 -0300

    Add files via upload

---
+++ build/precache-manifest.ea8883c226731e930fe9c5848e8b0ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d81c048fa556d0e4584f99890dac3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09a84b3381859d54c4e",
+    "url": "/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "d0daee22fdb165f58ca5",
+    "url": "/static/js/2.8a0d6a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a0d6a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09a84b3381859d54c4e",
+    "url": "/static/js/main.ad0e6df4.chunk.js"
+  },
+  {
+    "revision": "179522843cc72d53eef5",
+    "url": "/static/js/runtime-main.1f26698a.js"
+  }
+]);
